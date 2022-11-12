4fb23752ca7f3133038b0b040946967c4e2bdd55
AHardiment <99344068+AHardiment@users.noreply.github.com>
2022-11-12 13:45:42 +0000

    changed error page

--- middleware/route-guard.js
+++ middleware/route-guard.js
@@ -7,7 +7,7 @@ module.exports = (req, res, next) => {
   if (req.user) {
     next();
   } else {
-    const error = new Error('AUTHENTICATION_REQUIRED');
+    const error = new Error('You must be signed in');
     error.status = 401;
     next(error);
   }
